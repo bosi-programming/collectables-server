25b1239b8b9ac2fefcc6830be22662b834852662
bosifullstack <bosiarquitetura@gmail.com>
2023-07-08 19:49:58 -0300

    adiciona categoria e subcategoria

--- models/collectable.go
+++ models/collectable.go
@@ -8,6 +8,8 @@ type Collectable struct {
 	gorm.Model
 	Title       string `gorm:"size:255;not null" json:"title" binding:"required"`
 	Author      string `gorm:"size:255;not null" json:"author" binding:"required"`
+	Category      string `gorm:"size:255;not null" json:"category" binding:"required"`
+	SubCategory      string `gorm:"size:255;not null" json:"subCategory" binding:"required"`
 	PlaceOfCollectable string `gorm:"size:255;not null" json:"placeOfCollectable" binding:"required"`
 	UserID        int   `json:"user_id" binding:"required"`
 	User        *User   `json:"user" binding:"required"`
